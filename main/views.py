71b4947f4d09c076292c6fa361b2d7a16318055b
japawka <japawka@poczta.onet.pl>
2020-04-14 16:30:22 +0200

    css

--- main/views.py
+++ main/views.py
@@ -3,14 +3,36 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 from .forms import CreateNewList
 
+
+
 def index(request, id):
     list = ToDoList.objects.get(id=id)
     items = list.item_set.all()
+
+    if request.method == 'POST':
+        if request.POST.get('save'):
+            for item in items:
+                if request.POST.get('c' + str(item.id)) =='clicked':
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif request.POST.get('newItem'):
+            txt = request.POST.get('new')
+
+            if len(txt) > 2:
+                list.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
     context = {
         'list': list,
         'items': items
     }
     return render(request, 'main/list.html', context)
+
+
 
 
 def home(request):
@@ -22,6 +44,8 @@ def create(request):
         form.save()
         return redirect(all)
     return render(request, "main/create.html", {"form": form})
+
+
 
 def all(request):
      todos = ToDoList.objects.all()
